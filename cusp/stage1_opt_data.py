1f11f76bdc922791ab1ba61a619f6e3aff38ea7f
olsonjonny <olson.jonathanp@gmail.com>
2018-07-25 21:59:35 +0000

    Stage 1 Multithread test

---
+++ cusp/stage1_opt_data.py
@@ -0,0 +1,61 @@
+
+"""Driver routine for Stage One of CUSP: Training Set Preparation."""
+
+import settings
+import cusp_stage1
+import numpy as np
+from scipy.optimize import minimize
+
+from cusp_demo_utils import *
+
+
+# Settings for running the QAE optimization
+num_trials = settings.num_trials
+bond_lengths = settings.bond_lengths
+no_noise = settings.no_sampling_noise
+gate_error = settings.gate_error
+alpha = 'alpha'
+var_param = [alpha]
+
+# A wrapper to make sure that the selection of var_param above is properly resolved when input to the function
+num_param = len(var_param)
+fixed_vals = [0, 0, 0]
+all_param = [alpha]
+
+
+def run_state_preparation_optimization(bond_length):
+    """Runs optimization for VQE or state preparation.
+
+    Args:
+    =====
+    bond_length : float
+        Bond length of system-of-interest
+
+    Returns:
+    ========
+    optimized_params : numpy.ndarray
+        Vector of optimized state preparation parameters
+    """
+    # Initialize parameters
+    half_turn_min = 0
+    half_turn_max = 2
+    init_params = np.random.uniform(low=half_turn_min, high=half_turn_max, size=num_param)
+    result_list = []
+    
+    # Set up cost function
+    def stage1(lst, N=num_trials):
+        nonlocal result_list
+        input_list = fix_list(lst, all_param_array=all_param, var_param_array=var_param, fixed_vals_array=fixed_vals)
+        energy = cusp_stage1.compute_stage1_cost_function(*input_list, bond_length, n_repetitions=N,
+                                                          exact=no_noise, noisy=gate_error)
+        result_list.append(energy)
+        print(energy)
+        return energy
+
+    # Minimize using Nelder-Mead
+    res = minimize(stage1, init_params, args=(),
+                   method='Nelder-Mead', tol=None, 
+                   options={'disp': False, 'maxiter': 10,
+                   'xatol': 0.001, 'return_all': False, 'fatol': 0.001})
+    np.savetxt('stage1_data_'+str(bond_length)+'.csv',result_list, delimiter=',')
+    return res.x
---
+++ cusp_data_stage1.py
@@ -0,0 +1,61 @@
+import numpy as np
+import time
+from multiprocessing import Pool
+from scipy.optimize import minimize
+
+### SETTINGS ###
+num_trials = 100
+include_gate_noise = True
+noise_level = 0.003
+
+bond_lengths = [1.0]
+
+user_settings = np.array([True, include_gate_noise, noise_level,
+                          num_trials, bond_lengths], dtype=object)
+np.save('data/user_settings', user_settings)
+
+###STAGE 1###
+import sys
+from config import CODE_DIRECTORY
+sys.path.append(CODE_DIRECTORY)
+
+# User settings for CUSP
+import settings
+from cusp_demo_utils import *
+import cusp_stage1
+import stage1_opt_data
+
+print('#### STAGE 1 OF CUSP NOW RUNNING ####\n')
+
+# Lists to store energies
+check_energies = []       # Energies of FCI/exact wavefunctions
+stage1_energies = []      # Energies of VQE wavefunctions
+
+# Run thru bond lengths (or the training set)
+for bond_length in bond_lengths:
+    
+    # Run VQE calculation for each training point/state
+    opt_stage1_params = stage1_opt_data.run_state_preparation_optimization(bond_length)
+    print('Optimizing for bond length {0} ... '
+          'Optimal parameter setting is: {1}'.format(bond_length, opt_stage1_params))
+
+    # Compute and store energies to check results
+    exact_energy = settings.fetch_ground_energy(bond_length)
+    check_energies.append(exact_energy)
+    opt_energy = cusp_stage1.compute_stage1_cost_function(opt_stage1_params,
+                                                          bond_length,
+                                                          n_repetitions=num_trials,
+                                                          exact=True,
+                                                          noisy=include_gate_noise)
+    stage1_energies.append(opt_energy)
+    
+    # Display stage 1 results
+    print('Exact ground state energy               : {}'.format(exact_energy))
+    print('VQE optimized energy                    : {}'.format(opt_energy))
+    print('Energy difference (absolute value)      : {}\n'.format(
+            np.abs(opt_energy - exact_energy)))
+    
+    # Save these optimized VQE parameters into numpy arrays
+    np.save('data/stage1_param_{}'.format(bond_length), opt_stage1_params)
+    
+print(now)
